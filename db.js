c2545b8a0af447c894c7b5b7d65d681eca49f618
miguecg02 <miguecg02@correo.ugr.es>
2025-08-30 13:14:04 +0200

    Reedicion .env

--- db.js
+++ db.js
@@ -2,11 +2,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: '127.0.0.1',   
-  port: 3306,           // puerto por defecto de MySQL
-  user: 'myUserBD',
-  password: '2002',
-  database: 'GestionEnMovilidad_BD',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'myUserBD',
+  password: process.env.DB_PASSWORD || '2002',
+  database: process.env.DB_NAME || 'GestionEnMovilidad_BD',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -14,11 +14,11 @@ const pool = mysql.createPool({
 
 pool.getConnection()
   .then(conn => {
-    console.log('MySQL: conexión establecida vía TCP.');
+    console.log(`✅ MySQL: conexión establecida con ${process.env.DB_HOST || 'localhost'}`);
     conn.release();
   })
   .catch(err => {
-    console.error('MySQL: no pudo conectar vía TCP:', err);
+    console.error('❌ MySQL: no pudo conectar vía TCP:', err);
   });
 
 module.exports = {
