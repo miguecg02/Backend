99190881297d5bcef99cf11370a49acc7f38020d
miguecg02 <miguecg02@correo.ugr.es>
2025-08-30 14:12:59 +0200

    Reedicion server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const cors = require('cors');
 const personasRouter = require('./routes/PersonaEnMovilidad');
 const loginRoutes = require('./routes/Login');
